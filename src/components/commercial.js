7dd0b0645fc8d95c9ec661c9d3cdd0f8c9465211
RachedBenNasr <110614420+RachedBenNasr@users.noreply.github.com>
2024-04-20 18:05:55 +0100

    Website finished, entering testing phase.

---
+++ src/components/commercial.js
@@ -0,0 +1,214 @@
+import React, { useEffect, useState } from "react";
+import { getDatabase, ref, onValue } from "firebase/database";
+
+import "./vente.css";
+
+import ByuingListing from "../components/buying-listing";
+import Details from "../components/decisionDetails";
+
+const Vente = (props) => {
+  const [detailsVisible, setDetailsVisible] = useState(false);
+  const [selectedListing, setSelectedListing] = useState(null);
+  const [requestedListings, setRequestedListings] = useState([]);
+  const [approvedListings, setApprovedListings] = useState([]);
+  const [deniedListings, setDeniedListings] = useState([]);
+
+  const handleListingClick = (listing) => {
+    setSelectedListing(listing);
+    setDetailsVisible(true);
+  };
+
+  const handleCloseDetails = () => {
+    setDetailsVisible(false);
+    setSelectedListing(null);
+  };
+
+  useEffect(() => {
+    const fetchCommercialListings = async () => {
+      const database = getDatabase();
+      const saleListingsRef = ref(database, "listings/sale");
+      const rentListingsRef = ref(database, "listings/rent");
+
+      const fetchListings = (listingsRef) => {
+        return new Promise((resolve, reject) => {
+          onValue(
+            listingsRef,
+            (snapshot) => {
+              const data = snapshot.val();
+              if (data) {
+                resolve(Object.values(data));
+              } else {
+                resolve([]);
+              }
+            },
+            (error) => {
+              reject(error);
+            }
+          );
+        });
+      };
+
+      try {
+        const saleListings = await fetchListings(saleListingsRef);
+        const rentListings = await fetchListings(rentListingsRef);
+
+        const combinedListings = [...saleListings, ...rentListings];
+
+        const deniedListingsArray = [];
+        const approvedListingsArray = [];
+        const requestedListingsArray = [];
+
+        combinedListings.forEach((listing) => {
+          if (listing.state === "denied" && listing.nature == "commercial") {
+            deniedListingsArray.push(listing);
+          } else if (
+            listing.state === "approved" &&
+            listing.nature === "commercial"
+          ) {
+            approvedListingsArray.push(listing);
+          } else if (
+            listing.state === "requested" &&
+            listing.nature === "commercial"
+          ) {
+            requestedListingsArray.push(listing);
+          }
+        });
+
+        setDeniedListings(deniedListingsArray);
+        setApprovedListings(approvedListingsArray);
+        setRequestedListings(requestedListingsArray);
+      } catch (error) {
+        console.error("Error fetching listings:", error);
+      }
+    };
+
+    fetchCommercialListings();
+  }, []);
+
+  return (
+    <div className="vente-container">
+      <span className="vente-title">Commercial</span>
+      <div className="vente-separator"></div>
+      <div className="vente-list">
+        <span className="vente-sub">Annonces demandées</span>
+        <div className="vente-separator"></div>
+        <div className="cards">
+          {requestedListings.map((listing) => (
+            <div key={listing.id} onClick={() => handleListingClick(listing)}>
+              <ByuingListing
+                id={listing.id}
+                photos={listing.photos}
+                price={listing.price}
+                baths={listing.baths}
+                header={listing.header}
+                city={listing.city}
+                location={listing.location}
+                areaC={listing.areaC}
+                body={listing.body}
+                beds={listing.beds}
+                cars={listing.cars}
+                pool={listing.pool}
+                garden={listing.garden}
+                nature={listing.nature}
+                interval={listing.interval}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="vente-list">
+        <div className="vente-separator"></div>
+        <span className="vente-sub">Annonces approuvées</span>
+        <div className="vente-separator"></div>
+        <div className="cards">
+          {approvedListings.map((listing) => (
+            <div key={listing.id} onClick={() => handleListingClick(listing)}>
+              <ByuingListing
+                id={listing.id}
+                photos={listing.photos}
+                price={listing.price}
+                baths={listing.baths}
+                header={listing.header}
+                city={listing.city}
+                location={listing.location}
+                areaC={listing.areaC}
+                body={listing.body}
+                beds={listing.beds}
+                cars={listing.cars}
+                pool={listing.pool}
+                garden={listing.garden}
+                nature={listing.nature}
+                interval={listing.interval}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="vente-list">
+        <div className="vente-separator"></div>
+        <span className="vente-sub">Annonces refusées</span>
+        <div className="vente-separator"></div>
+        <div className="cards">
+          {deniedListings.map((listing) => (
+            <div key={listing.id} onClick={() => handleListingClick(listing)}>
+              <ByuingListing
+                id={listing.id}
+                photos={listing.photos}
+                price={listing.price}
+                baths={listing.baths}
+                header={listing.header}
+                city={listing.city}
+                location={listing.location}
+                areaC={listing.areaC}
+                body={listing.body}
+                beds={listing.beds}
+                cars={listing.cars}
+                pool={listing.pool}
+                garden={listing.garden}
+                nature={listing.nature}
+                interval={listing.interval}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+      {detailsVisible && (
+        <>
+          <div className="overlay" onClick={handleCloseDetails}></div>
+
+          <Details
+            listingId={selectedListing.id}
+            title={selectedListing.header}
+            photos={selectedListing.photos}
+            price={selectedListing.price}
+            baths={selectedListing.baths}
+            header={selectedListing.header}
+            location={selectedListing.location}
+            areaNC={selectedListing.areaNC}
+            areaC={selectedListing.areaC}
+            body={selectedListing.body}
+            beds={selectedListing.beds}
+            garage={selectedListing.garage}
+            pool={selectedListing.pool}
+            garden={selectedListing.garden}
+            nature={selectedListing.nature}
+            interval={selectedListing.interval}
+            closeDetails={handleCloseDetails}
+            type={selectedListing.type}
+            city={selectedListing.city} // Added
+            commercialType={selectedListing.commercialType} // Added
+            dateTime={selectedListing.dateTime} // Added
+            email={selectedListing.email} // Added
+            phoneNumber={selectedListing.phoneNumber} // Added
+            propertyTitle={selectedListing.propertyTitle} // Added
+            state={selectedListing.state} // Added
+            viabilise={selectedListing.viabilise} // Added
+            yearBuilt={selectedListing.yearBuilt}
+          />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Vente;
--- src/views/data.js
+++ src/views/data.js
@@ -5,7 +5,7 @@ import { getDatabase, ref, get } from "firebase/database";
 import Resume from "../components/resume";
 import Vente from "../components/vente";
 import Loyer from "../components/loyer";
-import Opportunite from "../components/opportunites";
+import Opportunite from "../components/commercial";
 import Feedback from "../components/feedback";
 
 import { Helmet } from "react-helmet";
@@ -27,6 +27,10 @@ const Data = () => {
   };
 
   const [unseenRequests, setUnseenRequests] = useState(0);
+
+  const [totalSaleRequests, setTotalSaleRequests] = useState(0);
+  const [totalRentRequests, setTotalRentRequests] = useState(0);
+  const [totalCommRequests, setTotalCommRequests] = useState(0);
 
   useEffect(() => {
     const fetchNumberOfRequests = async () => {
@@ -77,6 +81,106 @@ const Data = () => {
 
     fetchNumberOfRequests();
 
+    const fetchSaleRequests = async () => {
+      const database = getDatabase();
+      const saleListingsRef = ref(database, "listings/sale");
+
+      let temp = 0;
+
+      try {
+        const saleListingsSnapshot = await get(saleListingsRef);
+        const saleListingsData = saleListingsSnapshot.val();
+
+        if (saleListingsData) {
+          const listingsArray = Object.values(saleListingsData);
+          temp = listingsArray.reduce((count, listing) => {
+            if (listing.state === "requested" && listing.commercialType == "") {
+              return count + 1;
+            }
+            return count;
+          }, 0);
+        }
+      } catch (error) {
+        console.error("Error fetching sale listings:", error);
+      }
+
+      setTotalSaleRequests(temp);
+    };
+
+    const fetchRentRequests = async () => {
+      const database = getDatabase();
+      const saleListingsRef = ref(database, "listings/rent");
+
+      let temp = 0;
+
+      try {
+        const saleListingsSnapshot = await get(saleListingsRef);
+        const saleListingsData = saleListingsSnapshot.val();
+
+        if (saleListingsData) {
+          const listingsArray = Object.values(saleListingsData);
+          temp = listingsArray.reduce((count, listing) => {
+            if (listing.state === "requested" && listing.commercialType == "") {
+              return count + 1;
+            }
+            return count;
+          }, 0);
+        }
+      } catch (error) {
+        console.error("Error fetching rent listings:", error);
+      }
+
+      setTotalRentRequests(temp);
+    };
+
+    const fetchCommRequests = async () => {
+      const database = getDatabase();
+      const saleListingsRef = ref(database, "listings/sale");
+      const rentListingsRef = ref(database, "listings/rent");
+
+      let temp1 = 0;
+      let temp2 = 0;
+
+      try {
+        const saleListingsSnapshot = await get(saleListingsRef);
+        const saleListingsData = saleListingsSnapshot.val();
+
+        if (saleListingsData) {
+          const listingsArray = Object.values(saleListingsData);
+          temp1 = listingsArray.reduce((count, listing) => {
+            if (listing.state === "requested" && listing.commercialType != "") {
+              return count + 1;
+            }
+            return count;
+          }, 0);
+        }
+      } catch (error) {
+        console.error("Error fetching rent listings:", error);
+      }
+      try {
+        const rentListingsSnapshot = await get(rentListingsRef);
+        const rentListingsData = rentListingsSnapshot.val();
+
+        if (rentListingsData) {
+          const listingsArray = Object.values(rentListingsData);
+          temp2 = listingsArray.reduce((count, listing) => {
+            if (listing.state === "requested" && listing.commercialType != "") {
+              return count + 1;
+            }
+            return count;
+          }, 0);
+        }
+      } catch (error) {
+        console.error("Error fetching rent listings:", error);
+      }
+
+      setTotalCommRequests(temp1 + temp2);
+    };
+
+    fetchRentRequests();
+    fetchSaleRequests();
+    fetchCommRequests();
+
     //Fetch every 2 mins
     const intervalId = setInterval(fetchNumberOfRequests, 60 * 1000);
 
@@ -108,9 +212,13 @@ const Data = () => {
           onClick={() => handleMenuItemClick("vente")}
         >
           <span className="data-text3 not-selected">Vente</span>
-          <svg viewBox="0 0 877.7142857142857 1024" className="data-icon">
-            <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
-          </svg>
+          {totalSaleRequests != 0 && (
+            <>
+              <svg viewBox="0 0 877.7142857142857 1024" className="data-icon">
+                <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+              </svg>
+            </>
+          )}
         </div>
         <div
           className={`data-container2 ${
@@ -119,20 +227,28 @@ const Data = () => {
           onClick={() => handleMenuItemClick("loyer")}
         >
           <span className="not-selected">Loyer</span>
-          <svg viewBox="0 0 877.7142857142857 1024" className="data-icon2">
-            <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
-          </svg>
+          {totalRentRequests != 0 && (
+            <>
+              <svg viewBox="0 0 877.7142857142857 1024" className="data-icon">
+                <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+              </svg>
+            </>
+          )}
         </div>
         <div
           className={`data-container3 ${
-            selectedMenuItem === "opportunites" ? "selected" : "not-selected"
+            selectedMenuItem === "commercial" ? "selected" : "not-selected"
           }`}
-          onClick={() => handleMenuItemClick("opportunites")}
+          onClick={() => handleMenuItemClick("commercial")}
         >
           <span className="data-text5 not-selected">Commercial</span>
-          <svg viewBox="0 0 877.7142857142857 1024" className="data-icon4">
-            <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
-          </svg>
+          {totalCommRequests != 0 && (
+            <>
+              <svg viewBox="0 0 877.7142857142857 1024" className="data-icon">
+                <path d="M877.714 512c0 242.286-196.571 438.857-438.857 438.857s-438.857-196.571-438.857-438.857 196.571-438.857 438.857-438.857 438.857 196.571 438.857 438.857z"></path>
+              </svg>
+            </>
+          )}
         </div>
         <div
           className={`data-container4 ${
@@ -174,7 +290,7 @@ const Data = () => {
             <Route path="/data/resume" component={Resume} />
             <Route path="/data/vente" component={Vente} />
             <Route path="/data/loyer" component={Loyer} />
-            <Route path="/data/opportunites" component={Opportunite} />
+            <Route path="/data/commercial" component={Opportunite} />
             <Route path="/data/feedback" component={Feedback} />
 
             <Route
@@ -185,7 +301,7 @@ const Data = () => {
                     return <Vente />;
                   case "loyer":
                     return <Loyer />;
-                  case "opportunites":
+                  case "commercial":
                     return <Opportunite />;
                   case "feedback":
                     return <Feedback />;
